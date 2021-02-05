d1cdbc7dbc34b31ed01b461202a9fee20ff62a2c
sudo44 <sudo44@gmail.com>
2021-02-05 17:23:09 +0900

    delete unused files

---
+++ brewfile.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+if type brew > /dev/null 2>&1; then
+  echo 'OK! already installed brew.'
+else
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure using latest Homebrew
+echo 'start brew update...'
+brew update
+
+# Update already-installed formula
+echo 'start brew upgrade...'
+brew upgrade
+
+# Formula
+brew install ag
+brew install aws-sam-cli
+brew install awscli
+brew install git
+brew install jq
+brew install mas
+brew install mysql
+brew install nvm
+
+# .dmg apps
+
+brew install blackhole
+brew install clipy
+brew install deltawalker
+brew install docker
+brew install dropbox
+brew install firefox
+brew install forticlient
+brew install google-backup-and-sync
+brew install google-chrome
+brew install google-cloud-sdk
+brew install google-japanese-ime
+brew install karabiner-elements
+brew install kindle
+brew install obs
+brew install postman
+brew install sourcetree
+brew install spectacle
+# brew install virtualbox
+# brew install vagrant
+brew install visual-studio-code
+
+# appstore app
+mas install 539883307  # line
+mas install 409183694  # Keynote
+mas install 409201541  # Pages
+mas install 409203825  # Numbers
+
+
+# Remove outdated versions
+brew cleanup
---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+${PWD}/brewfile.sh
+
+# gitconfig
+if [ -e ~/.gitconfig ]; then
+	rm ~/.gitconfig
+fi
+if [ ! -L ~/.gitconfig ]; then
+	ln -s ${PWD}/gitconfig ~/.gitconfig
+fi
+if [ ! -L ~/.gitignore_global ]; then
+	ln -s ${PWD}/gitignore_global ~/.gitignore_global
+fi
+
